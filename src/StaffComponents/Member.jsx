75d3e0d60cde5dbf2c0c7eac78530c1cd74ecdbf
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-04-18 00:04:43 +0900

    スタイリング途中保存

--- src/StaffComponents/Member.jsx
+++ src/StaffComponents/Member.jsx
@@ -14,19 +14,21 @@ export const Member = (props) => {
     <div>
       <h2 className={`postName ${postClassName}`}>{ props.post }</h2>
       <section className="memberWrapper">
-        <div className="textWrapper">
-          <div className="nameTitle">
-            <div className="kindsPost">{ props.Department }</div>
-            <div className="staffName">{ props.name }</div>
+        <div className="staffWrapper">
+          <div className="textWrapper">
+            <div className="nameTitle">
+              <div className="kindsPost">{ props.Department }</div>
+              <div className="staffName">{ props.name }</div>
+            </div>
+            <div className="textContainer">
+              <p className="careerText">{ props.career }</p>
+              <p className="beliefText">{ props.berief }</p>
+              <p className="ExcitedText">{ props.excited }</p>
+            </div>
           </div>
-          <div className="textContainer">
-            <p className="careerText">{ props.career }</p>
-            <p className="beliefText">{ props.berief }</p>
-            <p className="ExcitedText">{ props.excited }</p>
+          <div className="imageWrapper">
+            <img src={ Human } alt={ props.staffImage } className='staffImage' />
           </div>
-        </div>
-        <div className="imageWrapper">
-          <img src={ Human } alt={ props.staffImage } className='staffImage' />
         </div>
       </section>
     </div>
--- src/StaffComponents/Staff.jsx
+++ src/StaffComponents/Staff.jsx
@@ -5,11 +5,11 @@ import Member from './Member';
 
 const Staff = () => {
   const members = [
-    { post: '代表', Department: '代表取締役', name: '開発桃太郎', career:'経歴のテキスト', berief:'想いのテキスト', excited:'おもしろテキスト', staffImage:'代表の写真' },
-    { post: '社員', Department: '営業', name: '開発イヌ太郎', career:'経歴のテキスト', berief:'想いのテキスト', excited:'おもしろテキスト', staffImage:'いぬ' },
-    { post: '', Department: '営業', name: '開発サル太郎', career:'経歴のテキスト', berief:'想いのテキスト', excited:'おもしろテキスト', staffImage:'さる' },
-    { post: '', Department: '営業', name: '開発キジ太郎', career:'経歴のテキスト', berief:'想いのテキスト', excited:'おもしろテキスト', staffImage:'きじ' },
-    { post: '', Department: '営業', name: '開発オニ太郎', career:'経歴のテキスト', berief:'想いのテキスト', excited:'おもしろテキスト', staffImage:'おに' },
+    { post: '代表', Department: '代表取締役', name: '開発 桃太郎', career:'経歴のテキスト', berief:'想いのテキスト', excited:'おもしろテキスト', staffImage:'代表の写真' },
+    { post: '社員', Department: '営業', name: '開発 イヌ太郎', career:'経歴のテキスト', berief:'想いのテキスト', excited:'おもしろテキスト', staffImage:'いぬ' },
+    { post: '', Department: '営業', name: '開発 サル太郎', career:'経歴のテキスト', berief:'想いのテキスト', excited:'おもしろテキスト', staffImage:'さる' },
+    { post: '', Department: '営業', name: '開発 キジ太郎', career:'経歴のテキスト', berief:'想いのテキスト', excited:'おもしろテキスト', staffImage:'きじ' },
+    { post: '', Department: '営業', name: '開発 オニ太郎', career:'経歴のテキスト', berief:'想いのテキスト', excited:'おもしろテキスト', staffImage:'おに' },
   ];
   return (
     <div>
