6fcfaec31378ff33d2bf3c3edbae4813318ec14a
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-04-29 12:49:33 +0900

    IF作成完了

--- src/ContactComponents/Form.jsx
+++ src/ContactComponents/Form.jsx
@@ -5,22 +5,25 @@ export const Form = () => {
     <div className="formPage">
         <h1 className="contactTitle">お問い合わせ</h1>
         <div className="formContainer">
-          <form action="">
-            <div className="formName">
-              <label htmlFor="" className="nameLabel">お名前</label>
-              <input type="text" className="nameInput" />
+          <form className="formContent" action="">
+            <div className="formName formItem">
+              <label htmlFor="" className="nameLabel labelContent">お名前</label>
+              <input type="text" className="nameInput inputContent" />
             </div>
-            <div className="formMail">
-              <label htmlFor="" className="mailLabel">メールアドレス</label>
-              <input type="text" className="mailInput" />
+            <div className="formMail formItem">
+              <label htmlFor="" className="mailLabel labelContent">メールアドレス</label>
+              <input type="text" className="mailInput inputContent" />
             </div>
-            <div className="formTitle">
-              <label htmlFor="" className="labelTitle">タイトル</label>
-              <input type="text" className="titleInput" />
+            <div className="formTitle formItem">
+              <label htmlFor="" className="labelTitle labelContent">タイトル</label>
+              <input type="text" className="titleInput inputContent" />
             </div>
-            <div className="formTextarea">
-              <label htmlFor="" className="msgLabel">メッセージ</label>
-              <textarea name="" id="" cols="30" rows="10" className="msgTextarea"></textarea>
+            <div className="formTextarea formItem">
+              <label htmlFor="" className="msgLabel labelContent">メッセージ</label>
+              <textarea className="msgTextarea inputContent"></textarea>
+            </div>
+            <div className="buttonContainer">
+              <button className="formBtn">送信する</button>
             </div>
           </form>
         </div>
