34fba9c498873755cfd6056d26c8570dd6c1bafb
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-10-15 19:39:20 +0900

    ♻️ コードのリファクタリングexport defaultから名前付きexportへの変更

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
-import './App.css';
+import "./App.css";
 // import MediaQuery from "react-responsive";
-import { BrowserRouter, Route ,Switch } from 'react-router-dom';
-import { Header } from './HeaderComponents/Header';
-import { Footer } from './FooterComponents/Footer';
-import { Service } from './ServiceComponents/Service';
-import { About } from './AboutComponents/About';
-import Staff from './StaffComponents/Staff';
-import { Contact } from './ContactComponents/Contact';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Header } from "./HeaderComponents/Header";
+import { Footer } from "./FooterComponents/Footer";
+import { Service } from "./ServiceComponents/Service";
+import { About } from "./AboutComponents/About";
+import { Staff } from "./StaffComponents/Staff";
+import { Contact } from "./ContactComponents/Contact";
 
 function App() {
   return (
     <div className="main">
-    <BrowserRouter>
-      <div className="header">
-        <Header />
-      </div>
-      <div className="body">
-        <Switch>
-          <Route exact path="/"><Service /></Route>
-          <Route exact path="/about"><About /></Route>
-          <Route exact path="/staff"><Staff /></Route>
-          <Route exact path="/contact"><Contact /></Route>
-          <Route ><NotFound /></Route>
-        </Switch>
-      </div>
-    </BrowserRouter>
+      <BrowserRouter>
+        <div className="header">
+          <Header />
+        </div>
+        <div className="body">
+          <Switch>
+            <Route exact path="/">
+              <Service />
+            </Route>
+            <Route exact path="/about">
+              <About />
+            </Route>
+            <Route exact path="/staff">
+              <Staff />
+            </Route>
+            <Route exact path="/contact">
+              <Contact />
+            </Route>
+            <Route>
+              <NotFound />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
       <div className="footer">
         <Footer />
       </div>
@@ -41,6 +51,4 @@ function NotFound() {
   return <h1>多分パスちがうで？ようみてみ</h1>;
 }
 
-
 export default App;
-
